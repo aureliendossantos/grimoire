7a2b3fee892a581f48ed69a4fe83f45714412be4
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2023-03-03 15:38:15 +0100

    Remember navigation options; article max width

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,76 +1,102 @@
-import React, { Suspense, useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React, { Suspense, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import config from "../grimoire-config.json"
+import config from "../grimoire-config.json";
 
-import UserForm from "./components/UserForm"
-import Debug from './pages/Debug'
-import NotFound from './pages/NotFound'
+import UserForm from "./components/UserForm";
+import Debug from "./pages/Debug";
+import NotFound from "./pages/NotFound";
 
-const Grimoire = React.lazy(() => import('./pages/Grimoire'))
-const CardPage = React.lazy(() => import('./pages/CardPage'))
+const Grimoire = React.lazy(() => import("./pages/Grimoire"));
+const CardPage = React.lazy(() => import("./pages/CardPage"));
 
-export default function Main():JSX.Element {
-  const [username, setUsername] = useState("")
-  const [platform, setPlatform] = useState("psn")
-  const [accountID, setAccountID] = useState("")
+export default function Main(): JSX.Element {
+  const [username, setUsername] = useState("");
+  const [platform, setPlatform] = useState("psn");
+  const [accountID, setAccountID] = useState("");
   const [userGrimoire, setuserGrimoire] = useState({
-    "score": 0,
-    "cardCollection": [],
-    "cardToHide": [],
-    "bonuses": []
-  })
-  const [isLoaded, setIsLoaded] = useState(false)
-  const host = 'https://www.bungie.net/d1/Platform/Destiny/'
+    score: 0,
+    cardCollection: [],
+    cardToHide: [],
+    bonuses: [],
+  });
+  const [isLoaded, setIsLoaded] = useState(false);
+  const host = "https://www.bungie.net/d1/Platform/Destiny/";
+
+  // Navigation option (see Grimoire component)
+  const [view, setView] = useState("themes");
+  const [showAdvanced, setShowAdvanced] = useState(false);
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     (async () => {
       try {
-        if (username != '') {
-          const response = await fetch(host + (platform == "xbox" ? '1' : '2') + '/Stats/GetMembershipIdByDisplayName/' + username + '/', {headers: {'X-API-Key': config.apiKey}})
-          console.log(host + (platform == "xbox" ? '1' : '2') + '/Stats/GetMembershipIdByDisplayName/' + username + '/')
-          const data = await response.json()
-          setAccountID(data.Response)
+        if (username != "") {
+          const response = await fetch(
+            host +
+              (platform == "xbox" ? "1" : "2") +
+              "/Stats/GetMembershipIdByDisplayName/" +
+              username +
+              "/",
+            { headers: { "X-API-Key": config.apiKey } }
+          );
+          console.log(
+            host +
+              (platform == "xbox" ? "1" : "2") +
+              "/Stats/GetMembershipIdByDisplayName/" +
+              username +
+              "/"
+          );
+          const data = await response.json();
+          setAccountID(data.Response);
         }
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
-    })()
-  }, [username],
-  )
+    })();
+  }, [username]);
 
   useEffect(() => {
     (async () => {
       try {
-        if (accountID != '') {
-          const response = await fetch(host + 'Vanguard/Grimoire/2/' + accountID + '/', {headers: {'X-API-Key': config.apiKey}})
-          const data = await response.json()
-          setuserGrimoire(data.Response.data)
-          setIsLoaded(true)
+        if (accountID != "") {
+          const response = await fetch(
+            host + "Vanguard/Grimoire/2/" + accountID + "/",
+            { headers: { "X-API-Key": config.apiKey } }
+          );
+          const data = await response.json();
+          setuserGrimoire(data.Response.data);
+          setIsLoaded(true);
         }
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
-    })()
-  }, [accountID])
+    })();
+  }, [accountID]);
 
   return (
     <div>
       <Router>
-      <h1><Link to="/">Grimoire Explorer</Link></h1>
-      {isLoaded ?
-        <h2>Utilisateur : {username} ({userGrimoire.score} points)</h2>
-      : <h2>Utilisateur</h2>}
-      <UserForm
-        username={username}
-        platform={platform}
-        onSubmit={(username, platform) => {
-          setUsername(username)
-          setPlatform(platform)
-        }}
-      />
-      
+        <h1>
+          <Link to="/">Grimoire Explorer</Link>
+        </h1>
+        {isLoaded ? (
+          <h2>
+            Utilisateur : {username} ({userGrimoire.score} points)
+          </h2>
+        ) : (
+          <h2>Utilisateur</h2>
+        )}
+        <UserForm
+          username={username}
+          platform={platform}
+          onSubmit={(username, platform) => {
+            setUsername(username);
+            setPlatform(platform);
+          }}
+        />
+
         <Switch>
           <Route path="/:themeId/:pageId/:cardId">
             <Suspense fallback={<h3>Chargement...</h3>}>
@@ -79,7 +105,22 @@ export default function Main():JSX.Element {
           </Route>
           <Route path={["/:themeId/:pageId", "/:themeId", "/"]}>
             <Suspense fallback={<h3>Chargement...</h3>}>
-              <Grimoire isLoaded={isLoaded} userGrimoire={userGrimoire} />
+              <Grimoire
+                isLoaded={isLoaded}
+                userGrimoire={userGrimoire}
+                view={view}
+                showAdvanced={showAdvanced}
+                filter={filter}
+                onViewChange={(userInput) => {
+                  setView(userInput);
+                }}
+                onShowAdvancedChange={(userInput) => {
+                  setShowAdvanced(userInput);
+                }}
+                onFilterChange={(userInput) => {
+                  setFilter(userInput);
+                }}
+              />
             </Suspense>
           </Route>
           <Route path="/debug/">
@@ -91,5 +132,5 @@ export default function Main():JSX.Element {
         </Switch>
       </Router>
     </div>
-  )
+  );
 }
--- src/pages/Grimoire.tsx
+++ src/pages/Grimoire.tsx
@@ -1,63 +1,70 @@
-import React, { useState } from "react"
-import frCardDefinition from "../GrimoireCardDefinition-fr.json"
-import enGrimoireDefinition from "../GrimoireDefinition-en.json"
-import { GrimoireCardDefinition, GrimoireDefinition, UserGrimoire } from "../apiType"
-import Filters from "../components/Grimoire/Filters"
-import ThemesView from "../components/Grimoire/ThemesView"
-import CardsView from "../components/Grimoire/CardsView"
-import ListView from "../components/Grimoire/ListView"
+import React, { useState } from "react";
+import frCardDefinition from "../GrimoireCardDefinition-fr.json";
+import enGrimoireDefinition from "../GrimoireDefinition-en.json";
+import {
+  GrimoireCardDefinition,
+  GrimoireDefinition,
+  UserGrimoire,
+} from "../apiType";
+import Filters from "../components/Grimoire/Filters";
+import ThemesView from "../components/Grimoire/ThemesView";
+import CardsView from "../components/Grimoire/CardsView";
+import ListView from "../components/Grimoire/ListView";
 
-export default function Grimoire(props:Props):JSX.Element {
-  const [view, setView] = useState("themes")
-  const [showAdvanced, setShowAdvanced] = useState(false)
-  const [filter, setFilter] = useState("all")
-
-  const cardDefinition: GrimoireCardDefinition = frCardDefinition
-  const grimoireDefinition: GrimoireDefinition = enGrimoireDefinition
+export default function Grimoire(props: Props): JSX.Element {
+  const cardDefinition: GrimoireCardDefinition = frCardDefinition;
+  const grimoireDefinition: GrimoireDefinition = enGrimoireDefinition;
 
   return (
     <>
       <h2>Navigateur</h2>
       <Filters
-        view={view}
-        showAdvanced={showAdvanced}
-        filter={filter}
+        view={props.view}
+        showAdvanced={props.showAdvanced}
+        filter={props.filter}
         onViewChange={(userInput) => {
-          setView(userInput)
+          props.onViewChange(userInput);
         }}
         onShowAdvancedChange={(userInput) => {
-          setShowAdvanced(userInput)
+          props.onShowAdvancedChange(userInput);
         }}
         onFilterChange={(userInput) => {
-          setFilter(userInput)
+          props.onFilterChange(userInput);
         }}
       />
-      {view == "list" ?
+      {props.view == "list" ? (
         <ListView
           cardDefinition={cardDefinition}
           grimoireDefinition={grimoireDefinition}
         />
-      : view == "themes" ?
-          <ThemesView
-            grimoireDefinition={grimoireDefinition}
-            cardCollection={props.userGrimoire.cardCollection}
-            bonuses={props.userGrimoire.bonuses}
-            showAdvanced={showAdvanced}
-            filter={filter}
-          />
-        : <CardsView
-            cardDefinition={cardDefinition}
-            cardCollection={props.userGrimoire.cardCollection}
-            bonuses={props.userGrimoire.bonuses}
-            showAdvanced={showAdvanced}
-            filter={filter}
-          />
-      }
+      ) : props.view == "themes" ? (
+        <ThemesView
+          grimoireDefinition={grimoireDefinition}
+          cardCollection={props.userGrimoire.cardCollection}
+          bonuses={props.userGrimoire.bonuses}
+          showAdvanced={props.showAdvanced}
+          filter={props.filter}
+        />
+      ) : (
+        <CardsView
+          cardDefinition={cardDefinition}
+          cardCollection={props.userGrimoire.cardCollection}
+          bonuses={props.userGrimoire.bonuses}
+          showAdvanced={props.showAdvanced}
+          filter={props.filter}
+        />
+      )}
     </>
-  )
+  );
 }
 
 interface Props {
-  isLoaded: boolean,
-  userGrimoire: UserGrimoire,
+  isLoaded: boolean;
+  userGrimoire: UserGrimoire;
+  view: string;
+  showAdvanced: boolean;
+  filter: string;
+  onViewChange: (userInput: string) => void;
+  onShowAdvancedChange: (userInput: boolean) => void;
+  onFilterChange: (userInput: string) => void;
 }
